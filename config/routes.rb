5c984e700bf7a68717b813511874ee97aec14849
Rashmi <poddar.rashmi@gmail.com>
2017-05-04 11:05:58 +0530

    Completed advanced test setup

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,5 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root 'application#hello'
-=======
-
->>>>>>> static-pages
 
 end
